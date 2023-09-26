b6da1ec1f24b0badab31e3d24d94357910b86375
Alumnos <Alumnos@PC214>
2023-09-26 14:26:06 -0300

    Agregar archivos de proyecto.

---
+++ prymoyano26092023/Frmmain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace prymoyano26092023
+{
+    partial class Frmmain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnconectar = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnconectar
+            // 
+            this.btnconectar.Location = new System.Drawing.Point(12, 12);
+            this.btnconectar.Name = "btnconectar";
+            this.btnconectar.Size = new System.Drawing.Size(149, 23);
+            this.btnconectar.TabIndex = 0;
+            this.btnconectar.Text = "Conectar Base\r\n";
+            this.btnconectar.UseVisualStyleBackColor = true;
+            this.btnconectar.Click += new System.EventHandler(this.btnconectar_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(26, 38);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(309, 66);
+            this.lblresultado.TabIndex = 1;
+            this.lblresultado.Text = "Resultado";
+            // 
+            // Frmmain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btnconectar);
+            this.Name = "Frmmain";
+            this.Text = "Base de Datos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnconectar;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ prymoyano26092023/Frmmain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Data.OleDb;
+
+namespace prymoyano26092023
+{
+    public partial class Frmmain : Form
+    {
+        OleDbConnection conexionBD;
+        public Frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnconectar_Click(object sender, EventArgs e)
+        {
+            conexionBD = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source=C:\\Users\\Alumnos\\source\\repos\\prymoyano26092023\\prymoyano26092023\\base\\EL_CLUB.accdb");
+            
+            conexionBD.Open();//abrir base de datos
+
+            lblresultado.Text = "Conectado a la Base";
+            lblresultado.ForeColor = System.Drawing.Color.Green;
+
+
+        }
+    }
+}
